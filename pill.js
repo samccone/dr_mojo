08c410485637e8375fbe8f96d2b8dce73a3e2528
Sam Saccone <sam@mojotech.com>
2012-04-13 15:15:02 -0400

    pills now update the board with their current position

--- pill.js
+++ pill.js
@@ -1,9 +1,12 @@
 function Pill(board) {
   this.board        = board;
   this.position			= { x : 0 , y : 0 };
+  this.lastPosition = this.position;
   this.width				= 2;
   this.height				= 1;
   this.colors				= [ colors[Math.floor(Math.random()*colors.length)], colors[Math.floor(Math.random()*colors.length)] ];
+  this.board.board[this.position.x][this.position.y] = this.colors[0];
+  this.board.board[this.position.x + 1][this.position.y] = this.colors[1];
   this.draw();
 }
 
@@ -16,36 +19,39 @@ Pill.prototype.draw = function(){
 
 Pill.prototype.erase = function(){
 	ctx.clearRect(this.position.x * block_size, this.position.y * block_size, block_size * this.width, block_size * this.height);
+  this.board.board[this.position.x][this.position.y] = undefined;
+  this.board.board[this.position.x + 1][this.position.y] = undefined;
 }
 
 Pill.prototype.moveRight = function() {
 	if ( ( this.position.x + this.width ) < this.board.width ) {
-		this.updatePosition(this.position,this.width,this.height);
 		this.erase();
 		this.position.x += 1;
 		this.draw();
+    this.updatePosition();
 	}
 }
 
 Pill.prototype.moveDown = function() {
 	if ( this.position.y + this.height < this.board.height ) {
-		this.updatePosition(this.position,this.width,this.height);
 		this.erase();
 		this.position.y += 1;
 		this.draw();
+    this.updatePosition();
 	}
 }
 
 
 Pill.prototype.moveLeft = function() {
 	if ( ( this.position.x ) > 0 ) {
-		this.updatePosition(this.position,this.width,this.height);
 		this.erase();
 		this.position.x -= 1;
 		this.draw();
+    this.updatePosition();
 	}
 }
 
 Pill.prototype.updatePosition = function(last,lastWidth){
-
+  this.board.board[this.position.x][this.position.y] = this.colors[0];
+  this.board.board[this.position.x + 1][this.position.y] = this.colors[1];
 }
