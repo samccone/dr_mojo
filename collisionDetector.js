d1a1fc289314b19a07586c6f16342d1f6f7f9e9f
Sam Saccone <sam@mojotech.com>
2012-04-15 13:38:27 -0400

    remove inbounds logic from the collisionDetector

--- collisionDetector.js
+++ collisionDetector.js
@@ -5,16 +5,12 @@ function CollisionDetector(board) {
 CollisionDetector.prototype.canMove = function(position) {
   var canMove = true;
   for( var i = 0; i < 2 && canMove; ++i ){
-    canMove = this.inBounds(position[i].x, position[i].y);
+    canMove = this.board.inBounds(position[i].x, position[i].y);
     if( canMove ) {
       canMove = this.checkSpot(position[i].x, position[i].y);
     }
   }
   return canMove;
-}
-
-CollisionDetector.prototype.inBounds = function(x, y) {
-  return ( x >= 0 && x < this.board.width && y < this.board.height );
 }
 
 CollisionDetector.prototype.checkSpot = function(x, y) {
