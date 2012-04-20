ce9532a9bd3fc80dc58e55e1ef0c70608a8590e3
Sam Saccone <samccone@gmail.com>
2012-04-19 20:25:18 -0400

    add case for main for testing

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 var canvas      = document.getElementById('game');
-var ctx         = canvas.getContext('2d');
+var ctx         = canvas && canvas.getContext('2d');
 var colors      = ["green", "red" , "teal"];
 var block_size  = 35;
 var the_game;
 var board_size  = [8,16];
 
-canvas.setAttribute('width', board_size[0] * block_size + "px");
-canvas.setAttribute('height', board_size[1] * block_size + "px");
+if(canvas){
+	canvas.setAttribute('width', board_size[0] * block_size + "px");
+	canvas.setAttribute('height', board_size[1] * block_size + "px");
 
-window.onload = function(){
-	the_game = new Game();
-  the_game.start(300);
+	window.onload = function(){
+		the_game = new Game();
+	  the_game.start(300);
+	}
 }
