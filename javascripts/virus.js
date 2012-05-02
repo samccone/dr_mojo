a961e788253e60b6fe5127ab80f2cb76f997599e
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2012-05-02 16:46:29 -0400

    full virus loading

--- javascripts/virus.js
+++ javascripts/virus.js
@@ -1,5 +1,6 @@
-function Virus(game, level, randColor) {
+function Virus(game, level, virusNum) {
   this.type       = 'Virus';
+  this.virusNum   = virusNum;
   this.game		  = game;
   this.board      = game.board;
   this.detector   = game.detector;
@@ -9,13 +10,15 @@ function Virus(game, level, randColor) {
   this.colors     = undefined; // for compatibility with Pill object
   this.position   = {x : undefined, y : undefined};
 
-  this.setColor(randColor);
+  this.setColor();
   this.setMaxBounds();
   this.setPosition();
 }
 
-Virus.prototype.setColor = function(randColor) {
-  if (randColor < 4) {
+Virus.prototype.setColor = function() {
+  var randColor = this.virusNum;
+
+  if (randColor < 3) {
     this.color = this.game.colors[randColor];
   } else {
     this.color = this.game.colors[Math.floor( Math.random()*this.game.colors.length )];
@@ -24,16 +27,18 @@ Virus.prototype.setColor = function(randColor) {
 }
 
 Virus.prototype.setMaxBounds = function() {
-  if (this.level <= 4 ) { this.maxY = 6; }
-  else if (this.level <= 8) { this.maxY = 7; }
+  if (this.level <= 4 ) { this.maxY = 10; }
+  else if (this.level <= 8) { this.maxY = 9; }
   else if (this.level <= 12 ) { this.maxY = 8; }
-  else if (this.level <= 16 ) { this.maxY = 9; }
-  else { this.maxY = 10; }
+  else if (this.level <= 16 ) { this.maxY = 7; }
+  else { this.maxY = 6; }
+	// 
+	// this.maxY=15;
 }
 
 Virus.prototype.setPosition = function() {
   var randX = Math.floor(Math.random() * this.board.width);
-  var randY = Math.floor(Math.random() * this.maxY);
+  var randY = Math.floor(Math.random() * 13) + 3;  // we dont ever put anything in the first 4 rows
 
   if (this.board.occupied(randX, randY) === undefined) {
   	this.position.x = randX;
