51923df4a4e9695af2ece63d23ca02abf7f307f6
Craig P Jolicoeur <cpjolicoeur@gmail.com>
2012-05-02 14:21:36 -0400

    ability to draw a virus piece

--- javascripts/PieceDrawer.js
+++ javascripts/PieceDrawer.js
@@ -1,13 +1,43 @@
 var PieceDrawer = (function(){
   return {
-    drawPiece : function(x, y, color){
+    drawPiece : function(x, y, color, drawVirus){
+		console.log("** drawing piece at coords:", x, y);
       if(ctx) {
-        ctx.fillStyle = color;
-        ctx.beginPath();
-        ctx.arc(x * block_size + block_size/2,y * block_size + block_size/2, block_size/2, block_size/2, Math.PI*2, true);
-        ctx.closePath();
-        ctx.fill();
+		if (drawVirus === true) {
+			// draw an X if we are drawing a Virus
+			ctx.fillStyle = color;
+			ctx.fillRect(x*block_size, y*block_size, block_size, block_size);
+
+			ctx.fillStyle = "black";
+			ctx.beginPath();
+			ctx.moveTo(x*block_size, y*block_size);
+			ctx.lineTo(x*block_size + block_size, y*block_size);
+			ctx.lineTo(x*block_size + block_size, y*block_size + block_size);
+			ctx.closePath();
+			ctx.stroke();
+			ctx.beginPath();
+			ctx.moveTo(x*block_size+block_size, y*block_size);
+			ctx.lineTo(x*block_size + block_size, y*block_size + block_size);
+			ctx.lineTo(x*block_size, y*block_size+block_size);
+			ctx.closePath();
+			ctx.stroke();
+			ctx.beginPath();
+			ctx.moveTo(x*block_size, y*block_size);
+			ctx.lineTo(x*block_size, y*block_size+block_size);
+			ctx.closePath();
+			ctx.stroke();
+		} else {
+	        ctx.fillStyle = color;
+	        ctx.beginPath();
+	        ctx.arc(x * block_size + block_size/2,y * block_size + block_size/2, block_size/2, block_size/2, Math.PI*2, true);
+	        ctx.closePath();
+	        ctx.fill();
+		}
       }
-    }
+    },
+
+	drawVirus : function(position, color) {
+		this.drawPiece(position.x, position.y, color, true);
+	}
   }
 })();
