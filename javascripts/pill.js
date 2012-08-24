cea0922eed42a0fa34edcca69c2888b6df193597
Sam Saccone <sam@mojotech.com>
2012-08-24 17:30:54 -0400

    [formatting]

--- javascripts/pill.js
+++ javascripts/pill.js
@@ -1,68 +1,74 @@
 function Pill(board, detector, position, _colors) {
-  this.type             = "pill";
-  this.detector         = detector;
-  this.board            = board;
-  this.position         = position || [ {x : Math.floor(this.board.width/2) -1  , y : 0 }, {x : Math.floor(this.board.width/2) , y : 0 } ];
-  this.colors           = _colors || [ colors[Math.floor(Math.random()*colors.length)], colors[Math.floor(Math.random()*colors.length)] ];
-  this.collision        = false;
-  this.rotationState    = 0;
+  this.type = "pill";
+  this.detector = detector;
+  this.board = board;
+  this.position = position || [{
+    x: Math.floor(this.board.width / 2) - 1,
+    y: 0
+  }, {
+    x: Math.floor(this.board.width / 2),
+    y: 0
+  }];
+  this.colors = _colors || [colors[Math.floor(Math.random() * colors.length)], colors[Math.floor(Math.random() * colors.length)]];
+  this.collision = false;
+  this.rotationState = 0;
   this.updatePosition();
   this.draw();
 }
 
-Pill.prototype.draw = function(){
-  for( var i = 0; i < this.colors.length; ++i){
-    var piece_rotation = (this.rotationState + 2*i)%4;
+Pill.prototype.draw = function() {
+  for (var i = 0; i < this.colors.length; ++i) {
+    var piece_rotation = (this.rotationState + 2 * i) % 4;
     this.position[i] && this.colors[i] && PieceDrawer.drawPiece(this.position[i].x, this.position[i].y, this.colors[i], piece_rotation);
   }
 }
 
-Pill.prototype.erase = function(){
-  for( var i = 0; i < this.position.length; ++i) {
+Pill.prototype.erase = function() {
+  for (var i = 0; i < this.position.length; ++i) {
     this.position[i] && this.board.eraseSpot(this.position[i].x, this.position[i].y);
   }
 }
 
 Pill.prototype.rotateLeft = function() {
-  this.rotate((this.rotationState + 1)%4);
+  this.rotate((this.rotationState + 1) % 4);
 }
 
 Pill.prototype.rotateRight = function() {
-  this.rotate((this.rotationState - 1)%4);
+  this.rotate((this.rotationState - 1) % 4);
 }
 
-Pill.prototype.clonePosition = function(){
+Pill.prototype.clonePosition = function() {
   var clone = [];
-  for( var i = 0; i < this.position.length; ++i ){
+  for (var i = 0; i < this.position.length; ++i) {
     clone.push({});
-    for ( var k in this.position[i] ) {
+    for (var k in this.position[i]) {
       clone[i][k] = this.position[i][k];
     }
   }
   return clone;
 }
 
-Pill.prototype.rotate = function(to){
+Pill.prototype.rotate = function(to) {
   var pos = this.clonePosition();
-  switch(to) {
-    case -3:
-    case 1:
-      pos[1].x = pos[0].x;
-      pos[1].y = pos[0].y + 1;
+  switch (to) {
+  case -3:
+  case 1:
+    pos[1].x = pos[0].x;
+    pos[1].y = pos[0].y + 1;
     break;
-    case -2:
-    case 2:
-      pos[1].x = pos[0].x -1;
-      pos[1].y = pos[0].y;
+  case -2:
+  case 2:
+    pos[1].x = pos[0].x - 1;
+    pos[1].y = pos[0].y;
     break;
-    case -1:
-    case 3:
-      pos[1].x = pos[0].x;
-      pos[1].y = pos[0].y - 1;
+  case -1:
+  case 3:
+    pos[1].x = pos[0].x;
+    pos[1].y = pos[0].y - 1;
     break;
-    case 0:
-      pos[1].x = pos[0].x + 1;
-      pos[1].y = pos[0].y;
+  case 0:
+    pos[1].x = pos[0].x + 1;
+    pos[1].y = pos[0].y;
     break;
   }
   this.move(pos, to)
@@ -76,12 +82,12 @@ Pill.prototype.move = function(pos, rotation_state) {
   this.erase();
   var canMove = this.detector.canMove(pos);
   this.collision = !canMove;
-  if(canMove) {
-    if(typeof(rotation_state)!='undefined'){
+  if (canMove) {
+    if (typeof(rotation_state) != 'undefined') {
       this.rotationState = rotation_state
     }
-    for( var i = 0; i < this.position.length; ++i){
-      if(this.position[i]){
+    for (var i = 0; i < this.position.length; ++i) {
+      if (this.position[i]) {
         this.position[i].x = pos[i].x;
         this.position[i].y = pos[i].y;
       }
@@ -97,8 +103,8 @@ Pill.prototype.moveRight = function() {
   for (var i = 0; i < this.position.length; ++i) {
     if (this.position[i]) {
       toMove.push({
-        x : this.position[i].x + 1,
-        y : this.position[i].y
+        x: this.position[i].x + 1,
+        y: this.position[i].y
       });
     } else {
       toMove.push(undefined);
@@ -112,8 +118,8 @@ Pill.prototype.moveDown = function() {
   for (var i = 0; i < this.position.length; ++i) {
     if (this.position[i]) {
       toMove.push({
-        x : this.position[i].x,
-        y : this.position[i].y + 1
+        x: this.position[i].x,
+        y: this.position[i].y + 1
       });
     } else {
       toMove.push(undefined);
@@ -128,8 +134,8 @@ Pill.prototype.moveLeft = function() {
   for (var i = 0; i < this.position.length; ++i) {
     if (this.position[i]) {
       toMove.push({
-        x : this.position[i].x - 1,
-        y : this.position[i].y
+        x: this.position[i].x - 1,
+        y: this.position[i].y
       });
     } else {
       toMove.push(undefined);
@@ -139,9 +145,12 @@ Pill.prototype.moveLeft = function() {
 }
 
 Pill.prototype.updatePosition = function() {
-  for(var i = 0; i < this.position.length; ++i) {
+  for (var i = 0; i < this.position.length; ++i) {
     if (this.position[i]) {
-      this.board.board[this.position[i].x][this.position[i].y] = { pos : i, pill : this};
+      this.board.board[this.position[i].x][this.position[i].y] = {
+        pos: i,
+        pill: this
+      };
     }
   }
 }
