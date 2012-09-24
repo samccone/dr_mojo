a7773b9c535c196a78eb2d60b37a17c97d607404
Franco Brusatti <franco@mojotech.com>
2012-09-24 20:35:20 -0300

    Add scoring system

--- javascripts/level.js
+++ javascripts/level.js
@@ -1,9 +1,14 @@
+const SPEED_VEL = {'Low': 300, 'Med': 600, 'High': 900};
+
 function Level(n, s) {
   this.number = n;
   this.speed  = s;
 }
 
 Level.prototype.velocity = function() {
-  var speed_in_number = {'Low': 300, 'Med': 600, 'High': 900};
-  return speed_in_number[this.speed];
+  return SPEED_VEL[this.speed];
+}
+
+Level.prototype.virus_score = function(){
+  return SPEED_VEL[this.speed] / (SPEED_VEL['Low']/100);
 }
