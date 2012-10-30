9750b0f472a55cb7342e6e9f51aec41eceb22b1d
Franco Brusatti <franco@mojotech.com>
2012-10-30 16:38:10 -0300

    Change webServer by app in server file

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ var express = require('express'),
 
 var oneYear = 31557600000;
 
-var webServer = express.createServer();
+var app = express.createServer();
 
-webServer.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-webServer.set("view engine", "jade");
-webServer.set('view options', { layout: false });
+app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+app.set("view engine", "jade");
+app.set('view options', { layout: false });
 
-webServer.get('/play', function(req, res){
+app.get('/play', function(req, res){
   res.render('play_game');
 });
 
-webServer.get('/restart', function(req, res) {
+app.get('/restart', function(req, res) {
   res.render('play_game');
 });
 
-webServer.get('/', function(req, res){
+app.get('/', function(req, res){
   res.render('index');
 });
 
-webServer.listen(port);
+app.listen(port);
 
-console.log('Listening on ' + port + ' in ' + webServer.settings.env + ' mode ...');
+console.log('Listening on ' + port + ' in ' + app.settings.env + ' mode ...');
 console.log('Press Ctrl + C to stop.');
