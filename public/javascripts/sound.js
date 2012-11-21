40a8567be1e446b7696d348518c7559f6792adc1
Will Farrell <will@mojotech.com>
2012-11-21 17:50:34 -0500

    Fix in-game music and sound effects

--- public/javascripts/sound.js
+++ public/javascripts/sound.js
@@ -1,57 +1,67 @@
-window.Sound = (function () {
-  var RATE = 44100;
-  var BUFFER_SIZE = 1000;
-  var PI2 = Math.PI * 2;
-  var VOLUME = 0.1;
+window.Sound = (function() {
 
-  var buffers = {};
-  var ready = false;
-
-  var audioContext = window.AudioContext || window.webkitAudioContext ||
-    window.mozAudioContext;
-
-  if (audioContext) {
-    var ac = new audioContext();
-    ready = true;
-  }
-
-  var beep = function () {
-    if (!ready) {
-      return false;
-    }
-
-    var args = Array.prototype.slice.call(arguments);
-    var length = args.shift();
-    var now = ac.currentTime;
-    var node;
-
-    _.each(args, function(freq, index) {
-      buffers[freq] = buffers[freq] || prepareBuffer(freq);
-      node = ac.createBufferSource(0);
-      var start = node.start ? 'start' : 'noteOn';
-      var stop  = node.stop  ? 'stop'  : 'noteOff';
-      node.buffer = buffers[freq];
-      node.loop = true;
-      node.connect(ac.destination);
-      node[start](now + length * index);
-      node[stop](now + length * (index + 1));
-    })
-  }
-
-  var prepareBuffer = function (freq) {
-    var buffer = ac.createBuffer(1, BUFFER_SIZE, RATE);
-    var channel = buffer.getChannelData(0);
-    var step = freq / RATE;
-
-    for (var i=0; i < BUFFER_SIZE; i++) {
-      // square wave
-      channel[i] = Math.sin(i * step * PI2) > 0 ? VOLUME : -VOLUME;
-    }
-    return buffer;
-  }
+  var sounds = {
+    rotate: "/wav/rotate.wav",
+    move: "/wav/move.wav",
+    explode: "/wav/explode.wav",
+    fever: "/mp3/fever.mp3",
+    chill: "/mp3/chill.mp3"
+  };
 
   return {
-    rotate : function () { beep(0.03, 400, 420); },
-    move   : function () { beep(0.03, 400, 450); }
-  }
+
+    song: false,
+    canPlay: false,
+
+    init: function() {
+      var _this = this;
+      soundManager.setup({
+        url: '/flash/',
+        onready: function() {
+          _.each(sounds, function(url, id) {
+            soundManager.createSound({
+              id: id,
+              url: url
+            });
+          });
+          _this.canPlay = true;
+          _this.musicPlay();
+        }
+      });
+    },
+
+    rotate: function() {
+      soundManager.play("rotate");
+    },
+
+    move: function() {
+      soundManager.play("move");
+    },
+
+    explode: function() {
+      soundManager.play("explode");
+    },
+
+    musicSet: function(song) {
+      this.song = song;
+      if(this.canPlay) {
+        this.musicStop();
+        this.musicPlay();
+      }
+    },
+
+    musicPlay: function() {
+      if(this.song) {
+        soundManager.play(this.song, {
+          onfinish: this.musicPlay
+        });
+      }
+    },
+
+    musicStop: function() {
+      soundManager.stopAll();
+    }
+
+  };
+
 })();
